8d457d068e901664840ed0b0202b4c05dc8f3bdc
arnaldo.villarrubia@upr.edu <arnaldo.villarrubia@upr.edu>
2020-10-30 20:17:28 -0400

    Created a Registration Form list for the employees to access all the created forms and process them.

---
+++ Pages/Application/registerForm.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace intro_durs.Pages.Application
+{
+    public class registerFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
